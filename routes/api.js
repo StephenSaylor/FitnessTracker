277263805ac8930d9a911b5ab7754f1b48fdbcb4
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-07 17:44:21 -0400

    api routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,26 @@
-const db = require("../models")
-const router = require("express").Router()
+const db = require('../models')
+const router = require('express').Router()
 
-router.get("/api/workouts", (req, res) => {
+router.get('/api/workouts', (req, res) => {
   db.Workout.find({})
+  .then((dbWorkout) => {
+    console.log(dbWorkout)
+    res.json(dbWorkout)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 })
+
+router.post('/api/workouts', (req, res) => {
+  db.Workout.create(req.body)
+    .then((dbWorkout) => {
+      console.log(dbWorkout)
+      res.json(dbWorkout)
+    })
+    .catch((err) => {
+      res.status(400).json(err)
+    })
+})
+
+module.exports = router
