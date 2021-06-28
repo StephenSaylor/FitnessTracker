e9d3ffab47c9d12090f72784ca7354fe63f1289a
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-27 21:25:40 -0400

    added atlas

--- server.js
+++ server.js
@@ -16,14 +16,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes)
 
-mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitnessDB',
+  {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}`)
-}))
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
+//   .then(
+//     app.listen(PORT, () => {
+//       console.log(`App listening on port ${PORT}`)
+// }))
